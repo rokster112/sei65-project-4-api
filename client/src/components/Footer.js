6277a238d421a8d9192a2acc0eee72da19d8d49a
rokster112 <rokster112@gmail.com>
2022-10-11 17:04:08 +0100

    make it better

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,4 +1,5 @@
 import image from '../styles/images/linkedin.png'
+import GITHUB from '../styles/images/github.png'
 
 
 const Footer = () => {
@@ -6,7 +7,7 @@ const Footer = () => {
     <div className="footer-main">
       <div className="footer-header">Created by:</div>
       <div className='links'>
-        <div className="Rokas"><a className='a-rokas' href="https://github.com/rokster112" target="_blank" rel="noreferrer">Rokas Arlauskas -&gt; GitHub</a></div>
+        <div className='Rokas'><a className='a-rokas' href='https://github.com/rokster112'  target='_blank' rel='noreferrer'><img src={GITHUB} style={{ height: 58, width: 58 }}></img></a></div>
         <div className="Logo"><a href="https://www.linkedin.com/in/rokas-arlauskas-8772b6244/" target="_blank" rel="noreferrer"><img src={image} height="50px"></img></a></div>
       </div>
     </div>  
